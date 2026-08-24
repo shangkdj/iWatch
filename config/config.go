8ef8418ed1989de870d562aad16dca7805d6e0ba
“shangkdj” <624342043@qq.com>
2026-08-24 18:30:50 +0800

    change db

--- config/config.go
+++ config/config.go
@@ -1,33 +1,33 @@
 package config
 
 type Config struct {
-    // PostgreSQL
-    DBHost     string
-    DBPort     string
-    DBUser     string
-    DBPassword string
-    DBName     string
-    SSLMode    string
+	// PostgreSQL
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	SSLMode    string
 
-    // Redis
-    RedisAddr     string
-    RedisPassword string
-    RedisDB       int
+	// Redis
+	RedisAddr     string
+	RedisPassword string
+	RedisDB       int
 }
 
 func LoadConfig() Config {
-    return Config{
-        // PostgreSQL 配置（改成你自己的）
-        DBHost:     "localhost",
-        DBPort:     "5432",
-        DBUser:     "postgres",
-        DBPassword: "Admin%100", 
-        DBName:     "iWathch", 
-        SSLMode:    "disable",
+	return Config{
+		// PostgreSQL 配置（改成你自己的）
+		DBHost:     "localhost",
+		DBPort:     "5432",
+		DBUser:     "postgres",
+		DBPassword: "Admin%100",
+		DBName:     "health_app",
+		SSLMode:    "disable",
 
-        // Redis 配置
-        RedisAddr:     "localhost:6379",
-        RedisPassword: "Admin%100",
-        RedisDB:       0,
-    }
+		// Redis 配置
+		RedisAddr:     "localhost:6379",
+		RedisPassword: "Admin%100",
+		RedisDB:       0,
+	}
 }
